e7edd86e6b253bc108f5e0703916efa4ef42725c
Adeniji Charles Ayodipupo <adenijiayocharles@gmail.com>
2020-04-30 12:11:17 +0100

    fixes

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Welcome </title>
+    <style>
+        body {
+            background-color: rgb(124, 9, 128);
+            color: #FFF;
+        }
+
+        ol {
+            margin: auto;
+            list-style: none;
+            text-align: center;
+        }
+
+        ol > li {
+            margin: 0;
+        }
+
+        ol > li >a {
+            color: white;
+        }
+
+        div {
+            width: 300px;
+            text-align: center;
+            margin: auto;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <h4> Experiment Approval Application <br> RGU Approvr </h4>
+
+        <p> Click the links to access the application </p>
+        <ol>
+            <li><a href='/students' target="_blank"> Student </a></li>
+            <li><a href='/eao' target="_blank"> Experiment Approval Officer </a></li>
+            <li><a href='/administrator' target="_blank"> Administrator </a></li>
+        </ol>
+    </div>
+</body>
+</html>
